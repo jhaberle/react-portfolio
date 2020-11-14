9522cd768071db7f08035540b8d20b71d09590e6
Jake Haberle <jake.haberle@gmail.com>
2020-11-14 00:05:19 -0700

    created new react app and updated all files

--- reactport/src/Portfolio.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Portfolio = () => {
-  return <div>Howdy Yall</div>;
-};
-
-export default Portfolio;
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="row">
+        <div class="col-sm-12">
+          <footer>&copy&nbspChairmen Designs - 2020</footer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
